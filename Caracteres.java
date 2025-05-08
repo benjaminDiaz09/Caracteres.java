f307e3ae0f3520a931264e3a86f41e07da68ef68
benjaminDiaz09 <20243rd026@utez.edu.mx>
2025-05-07 22:14:02 -0600

    Original Caracteres

---
+++ Caracteres.java
@@ -0,0 +1,11 @@
+public class Caracteres {
+    public static void main (String [] args){
+    char letra = 'J';
+    char simbolo ='@' ;
+    char numero= '5';
+    
+    System.out.println("Letra=" + letra );
+    System.out.println("simbolo=" + simbolo );
+    System.out.println("numero=" + numero );
+    }
+}
